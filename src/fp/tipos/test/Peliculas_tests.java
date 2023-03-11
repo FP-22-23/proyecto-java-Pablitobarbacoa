106836ceff526d48791af975e424a771ce19345f
Pablitobarbacoa <pabperace@alum.us.es>
2023-03-11 20:08:04 +0100

    Terminado el constructor con todas las variables

--- src/fp/tipos/test/Peliculas_tests.java
+++ src/fp/tipos/test/Peliculas_tests.java
@@ -1,5 +1,6 @@
 package fp.tipos.test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 
 import java.util.ArrayList;
@@ -8,25 +9,42 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+import fp.tiposs.Categoria;
 import fp.tiposs.Peliculas;
 
 
 import fp.tiposs.PeliculasImpl;
 
+@SuppressWarnings("unused")
 public class Peliculas_tests {
 
+	public static void main(String []args) {
 	List<String> listita = new LinkedList<>();
 	
 	String star1= "Leonardo DiCaprio";
 	String star2="Margot Robbie";
 	
-	listita.add(e1);
-	listita.add(e2);
+	listita.add(star1);
+	listita.add(star2);
 	
+	
+	List<String> categorias = new LinkedList();
+	
+	String cat1= "Comedia negra";
+	String cat2= "Drama biográfico";
+	String cat3= "Thriller policial";
+	categorias.add(cat1);
+	categorias.add(cat2);
+	categorias.add(cat3);
+	
+	Categoria c= new Categoria(categorias, "Más de 18");
 			
 	
-	Peliculas p= new PeliculasImpl("El lobo de Wall Street", "Martin Scorsese", null, null, null, null, null, null);
+	Peliculas p= new PeliculasImpl("El lobo de Wall Street", "Martin Scorsese", listita, 8.2, Duration.ofMinutes(179), true, 392.5, c);
+	
+	System.out.println(p);
 	
 	
 	
+}
 }
